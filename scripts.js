dde4cc9c3ba20168c3c0de62bc5df47ed907ff40
Dan Vitoriano <vitoriano08@gmail.com>
2023-08-13 23:33:39 -0300

    node e console

--- scripts.js
+++ scripts.js
@@ -1,4 +1,2 @@
-alert("javascript externo")
-alert("javascript externo 2")
-alert("javascript externo 3")
-document.write("arquivo externo")
+// document.write("escrevi a partir do arquivo externo")
+console.log("teste")
