d20afef24ff57e8dd5079a5777d038075fa07551
Dan Vitoriano <vitoriano08@gmail.com>
2023-08-14 00:35:48 -0300

    07 - manipulando textos e numeros

--- scripts.js
+++ scripts.js
@@ -1,39 +1,61 @@
-// declaracao de variaveis
-let nome;
-let disciplina = null;
-let texto = "Este é um exemplo";
-console.log(texto)
-// document.write(texto)
+// parseInt
+let numFloat = 123.4551
+console.log(parseInt(numFloat))
 
-// tipos de dados
+// parseFloat
+let numString = "123.45"
+console.log(numString)
+console.log(parseFloat(numString))
 
-// numeros
-let idadeAluno = 18;
-console.log(idadeAluno)
+// toString
+console.log(numFloat.toString())
 
-// float
-let valorProduto = 5.35
-console.log(valorProduto)
+// typeof
+let obj = { nome: "danilo", idade: 42}
+let arr = [1,2,3,"teste"]
+let bool = true
 
-// booleano
-let casada = false
-console.log(casada)
+console.log(typeof numString)
+console.log(typeof numFloat)
+console.log(typeof obj)
+console.log(typeof arr)
+console.log(typeof bool)
 
-// listas (array)
-let linguagens = ["Java", "JS", "Python", 1, true, 4.56]
-console.log(linguagens)
+// caractere especial
+let pedido = 'Quero um copo d \'agua'
+console.log(pedido)
 
-// objetos
-let carro = {
-    cor: "vermelho",
-    numPortas: 4,
-    tipo: "sedan",
-    modelo: "Onix",
-    marca: "GM",
-    correr: function(){
-        alert("estou correndo!")
-    }
-}
-console.log(carro)
-console.log(carro.cor)
-console.log(carro.correr())
+// length
+console.log(pedido.length)
+console.log(numString.length)
+console.log(arr.length)
+console.log(obj.length)
+console.log(numFloat.length)
+console.log(bool.length)
+
+// indexOf
+console.log(pedido.indexOf("copo"))
+console.log(pedido.indexOf("a"))
+console.log(pedido.lastIndexOf("a"))
+
+// slice
+console.log(pedido.slice(0,5))
+console.log(pedido.slice(6,8))
+console.log(pedido)
+
+// substr
+console.log(pedido.substr(6,8))
+
+// replace
+console.log(pedido.replace("um", "algum"))
+console.log(pedido)
+
+// toUpperCase
+console.log(pedido.toUpperCase())
+
+// toLowerCase
+console.log(pedido.toLocaleLowerCase())
+
+// toFixed
+console.log(numFloat.toFixed(2))
+
